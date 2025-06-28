f90342c1b28e376897a00754ff198d667c16b853
narlon <narlonzjh@163.com>
2025-06-28 20:06:15 +0800

    init

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MainController : MonoBehaviour
+{
+    public TMP_Text questionText;
+    public GameObject[] panels;
+    private int correctAnswer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BindUI();
+        GenerateQuestion();
+    }
+
+    private void BindUI()
+    {
+        // 创建答案按钮
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject buttonObj = new GameObject("AnswerButton" + i);
+            // 确保按钮在 Canvas 下
+            if (transform.GetComponentInParent<Canvas>()) {
+                buttonObj.transform.SetParent(transform.GetComponentInParent<Canvas>().transform);
+            }
+            var answerButton = panels[i].GetComponent<Button>();
+            answerButton.onClick.AddListener(() => CheckAnswer(answerButton));
+        }
+    }
+
+    private void GenerateQuestion()
+    {
+        int num1 = Random.Range(1, 10);
+        int num2 = Random.Range(1, 10);
+        int op = Random.Range(0, 2);
+        string question = "";
+        if (op == 0)
+        {
+            correctAnswer = num1 + num2;
+            question = $"{num1} + {num2} = ?";
+        }
+        else
+        {
+            correctAnswer = num1 - num2;
+            question = $"{num1} - {num2} = ?";
+        }
+        questionText.text = question;
+
+        // 设置答案选项
+        List<int> answers = new List<int>();
+        answers.Add(correctAnswer);
+        while (answers.Count < 5)
+        {
+            int wrongAnswer = correctAnswer + Random.Range(-5, 6);
+            if (wrongAnswer != correctAnswer && !answers.Contains(wrongAnswer))
+            {
+                answers.Add(wrongAnswer);
+            }
+        }
+        answers = ShuffleList(answers);
+        for (int i = 0; i < 5; i++)
+        {
+            panels[i].GetComponentInChildren<TMP_Text>().text = answers[i].ToString();
+        }
+    }
+
+    private List<int> ShuffleList(List<int> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            int temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+        return list;
+    }
+
+    private void CheckAnswer(Button button)
+    {
+        Debug.Log("click");
+        int selectedAnswer = int.Parse(button.GetComponentInChildren<TMP_Text>().text);
+        if (selectedAnswer == correctAnswer)
+        {
+            // 答案正确，生成新问题
+            GenerateQuestion();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
